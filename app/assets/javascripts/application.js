9386ff2223134aae5e2166403a02ab5422716497
dangerstephen <dangersteve1@gmail.com>
2016-12-20 11:32:31 -0800

    added js, and css to work to change color and count and reset at refresh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
 
 var page_view_count = {
   "home": 0,
@@ -21,3 +5,7 @@ var page_view_count = {
   "lightcoral": 0,
   "lightseagreen": 0
 };
+
+// location.page_view_count.split("/")[1] = {
+// document.bgColor="#e7b82b"
+// }
